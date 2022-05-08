664d6f6d025ffd6b40f2482e2a30fd225afc6fe4
Evil0ctal <evil0ctal1985@gmail.com>
2022-05-08 01:25:19 -0700

    Update get_colleges_list.py

--- get_colleges_list.py
+++ get_colleges_list.py
@@ -1,5 +1,9 @@
+import time
+
 import requests
 import lxml
+import os
+import googlesearch
 from bs4 import BeautifulSoup
 
 
@@ -12,15 +16,29 @@ def get_100_colleges(url):
         i = i.text
         if '.' in i:
             cc_name = i.split('. ')[-1].replace('\n', '')
-            colleges.append(cc_name)
+            colleges.append(cc_name + ' computer science')
+            with open('colleges_list.txt', 'a', encoding='utf-8') as f:
+                f.write(cc_name + ' computer science' + '\n')
+    print(len(colleges))
     return colleges
 
 
-def get_colleges_website(colleges_list: list):
-    print(colleges_list)
+def get_colleges_website():
+    with open('colleges_list.txt', 'r', encoding='utf-8') as f:
+        colleges_list = f.readlines()
+    index = 0
+    for i in colleges_list:
+        index += 1
+        result = list(googlesearch.search(i, num_results=1))[0]
+        with open('used_list.txt', 'a', encoding='utf-8') as f:
+            f.write(i)
+        with open('websites_list.txt', 'a', encoding='utf-8') as f:
+            f.write(i)
+            f.write(result + '\n\n')
+        print("Index: {} \nCollege: {}Result: {}\n".format(index, i, result))
 
 
 if __name__ == '__main__':
     colleges_list_url = "https://stacker.com/stories/1803/100-best-community-colleges-america"
-    colleges_list = get_100_colleges(colleges_list_url)
-    get_colleges_website(colleges_list)
+    # colleges_list = get_100_colleges(colleges_list_url)
+    get_colleges_website()
